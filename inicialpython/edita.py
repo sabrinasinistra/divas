5bd988b55a9d53b23fa5080f08a79be9c4da1a2f
mathss7 <mv7423623@gmail.com>
2025-04-21 22:04:25 -0300

    exemplo em python

---
+++ inicialpython/edita.py
@@ -0,0 +1,12 @@
+arquivo = open("dadosleitura.txt", "a")
+
+for i in range(8):
+    if(i == 1):
+        arquivo.write("Sabrina linda")
+    else:
+        arquivo.write(" Sabrina linda ")
+
+
+
+
+
